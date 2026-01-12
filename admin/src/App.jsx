824204fa2797dfee8a780ea1c9302bbb547d0054
Paulo-Sergio-Oliveira <paulosergiooliveira113@gmai.com>
2026-01-12 19:19:38 -0300

    initialize admin panel with React and Vite

---
+++ admin/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1>Painel Administrativo</h1>
+  )
+}
+
+export default App
--- vitrine/src/App.jsx
+++ vitrine/src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 
-<h1>Vitrine da Loja</h1>
+function App() {
+    return (
+        <h1>Vitrine da Loja</h1>
+    )
+}
 
 export default App
